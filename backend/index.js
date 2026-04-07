879610633e9afceb286d198600ee03416369c53f
ag630zt <adam.griger@student.tuke.sk>
2026-04-08 01:06:58 +0200

    setup for render

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,15 @@ import cors from "cors";
 import authRoutes from "./src/routes/auth.js";
 import companiesRoutes from "./src/routes/companies.js";
 import usersRoutes from "./src/routes/users.js";
-import { generatePassword } from "./src/services/user.js";
 
 const app = express();
 app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("API beží 🚀");
+});
 
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.static("public"));
@@ -15,6 +20,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/companies", companiesRoutes);
 app.use("/api/users", usersRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
 });
